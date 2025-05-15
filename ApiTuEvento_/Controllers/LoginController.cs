76dbbdaf40f97501b61d3fd1d607a3be0f4c4716
CamiLoP19 <janercpacheco@unicesar.edu.co>
2025-05-15 15:46:42 -0500

    coomit 2

---
+++ ApiTuEvento_/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using ApiTuEvento_.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.EntityFrameworkCore;
+using WebApiRest.Helpers;
+
+namespace ApiTuEvento_.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ContextDB _context;
+        private readonly JwtHelper _jwtHelper;
+
+        public LoginController(ContextDB context, JwtHelper jwtHelper)
+        {
+            _context = context;
+            _jwtHelper = jwtHelper;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var user = await _context.usuarios
+                .FirstOrDefaultAsync(u => u.NombreUsuario == loginDto.NombreUsuario);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginDto.Contraseña, user.Contraseña))
+            {
+                return Unauthorized("Credenciales inválidas");
+            }
+
+            var token = _jwtHelper.GenerateToken(user.NombreUsuario);
+
+            return Ok(new
+            {
+                token,
+                user = new
+                {
+                    user.NombreUsuario,
+                    user.Correo,
+                    user.Rol
+                }
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            if (_context.usuarios.Any(u => u.NombreUsuario == dto.NombreUsuario))
+                return BadRequest("El usuario ya existe.");
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(dto.Contraseña);
+
+            var user = new Usuario
+            {
+                Cedula = dto.Cedula,
+                Rol = dto.Rol,
+                NombreUsuario = dto.NombreUsuario,
+                Correo = dto.Correo,
+                Contraseña = hashedPassword,
+                FechaNacimiento = dto.FechaNacimiento
+            };
+
+            _context.usuarios.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("Usuario registrado correctamente.");
+        }
+    }
+
+}
+
---
+++ ApiTuEvento_/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿namespace ApiTuEvento_.Models
+{
+    public class Login
+    {
+
+    }
+
+    public class LoginDto
+    {
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+    }
+
+    public class RegisterDto
+    {
+        public double Cedula { get; set; }
+        public string Rol { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
